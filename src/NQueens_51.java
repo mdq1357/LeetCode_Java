1501d169776cf9cc822cff6dd2e7022b411dfb03
mdq1357 <mdq1357@163.com>
2022-01-12 20:48:38 +0800

    补充题目描述

--- src/NQueens_51.java
+++ src/NQueens_51.java
@@ -17,6 +17,15 @@ public class NQueens_51 {
 //        System.out.println(solveNQueens(1));
     }
 
+    //n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+    //
+    //给你一个整数 n ，返回所有不同的 n 皇后问题 的解决方案。
+    //
+    //每一种解法包含一个不同的 n 皇后问题 的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+    //
+    //来源：力扣（LeetCode）
+    //链接：https://leetcode-cn.com/problems/n-queens
+    //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
     public static List<List<String>> res=new ArrayList<>();       //结果集
 
     public static List<List<String>> solveNQueens(int n) {
--- src/openLock_752.java
+++ src/openLock_752.java
@@ -7,6 +7,19 @@ public class openLock_752 {
     public static void main(String[] args) {
         System.out.println(openLock(new String[] {"0201","0101","0102","1212","2002"}, "0202"));
     }
+
+    //打开转盘锁
+    //你有一个带有四个圆形拨轮的转盘锁。每个拨轮都有10个数字： '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' 。每个拨轮可以自由旋转：例如把 '9' 变为 '0'，'0' 变为 '9' 。每次旋转都只能旋转一个拨轮的一位数字。
+    //
+    //锁的初始数字为 '0000' ，一个代表四个拨轮的数字的字符串。
+    //
+    //列表 deadends 包含了一组死亡数字，一旦拨轮的数字和列表里的任何一个元素相同，这个锁将会被永久锁定，无法再被旋转。
+    //
+    //字符串 target 代表可以解锁的数字，你需要给出解锁需要的最小旋转次数，如果无论如何不能解锁，返回 -1 。
+    //
+    //来源：力扣（LeetCode）
+    //链接：https://leetcode-cn.com/problems/open-the-lock
+    //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
     public static int openLock(String[] deadends, String target) {
         Queue<String> q=new LinkedList<>();
         Set<String> visted=new HashSet<>();
