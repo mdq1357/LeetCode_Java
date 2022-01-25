61d5102659115e18e3e0de3f53daf90564f45175
mdq1357 <mdq1357@163.com>
2022-01-25 17:33:20 +0800

    打家劫舍

--- src/rob_337.java
+++ src/rob_337.java
@@ -15,17 +15,18 @@ public class rob_337 {
     //链接：https://leetcode-cn.com/problems/house-robber-iii
     //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
+    /**法一 */
     static Map<TreeNode,Integer> memo=new HashMap<>();
     public static int rob(TreeNode root) {
         if(root==null) return 0;
         if(memo.containsKey(root)) return memo.get(root);
-        //抢当前节点，然后找下下个节点
-        int do_it= root.val +(
-                root.left==null? 0 : rob(root.left.left)+rob(root.left.right)
+
+        int do_it=
+                root.val +(
+                        root.left==null? 0 : rob(root.left.left)+rob(root.left.right)
                 )+(
-                root.right==null? 0 : rob(root.right.left)+rob(root.right.right)
+                        root.right==null? 0 : rob(root.right.left)+rob(root.right.right)
                 );
-        //不抢当前节点，找子节点
         int not_do=rob(root.left)+rob(root.right);
 
         int res=Math.max(do_it,not_do);
@@ -33,6 +34,24 @@ public class rob_337 {
         return res;
     }
 
+    /**法二 */
+    public static int rob(TreeNode root) {
+        int[] res=dp(root);
+        return Math.max(res[0],res[1]);
+    }
+
+    public int[] dp(TreeNode root){ //res[0]代表不抢的结果，res[1]代表抢的结果
+        if(root==null) return new int[]{0,0};
+        int[] rob_left=dp(root.left);
+        int[] rob_right=dp(root.right);
+        //抢
+        int do_it=root.val+rob_left[0]+rob_right[0];
+        //不抢
+        int not_do=Math.max(rob_left[0],rob_left[1])+Math.max(rob_right[0],rob_right[1]);
+        return new int[]{not_do,do_it};
+    }
+
+    //TreeNode说明
     public class TreeNode {
         int val;
         TreeNode left;
